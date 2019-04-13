e7c1e75430b0cf38791fcb21617070d37acffc16
meanking <Myloveson2017630@outlook.com>
2019-04-14 05:40:11 +0900

    about, rentals, styling

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -7,7 +7,18 @@ module('Acceptance | list rentals', function(hooks) {
 
   test('visiting /', async function(assert) {
     await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 'should display 3 listings');
+  });
 
-    assert.equal(currentURL(), '/');
+  test('should link to information about the company', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 });
