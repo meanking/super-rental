9a817801fbbe412f5f0297c267eaed795e0a7733
meanking <Myloveson2017630@outlook.com>
2019-04-14 11:54:48 +0900

    install addon, simple component, handlebars helper

---
+++ tests/integration/components/rental-listing-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | rental-listing', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should display rental details', async function (assert) {
+
+  });
+
+  test('should toggle wide class on click', async function (assert) {
+
+  });
+});
