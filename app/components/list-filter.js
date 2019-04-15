b0b3f5e6856bad8c2209361f5d1f6357f45dc68a
meanking <Myloveson2017630@outlook.com>
2019-04-16 05:22:59 +0900

    filtering-complex component with promise

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,7 +6,9 @@ export default Component.extend({
 
     init() {
         this._super(...arguments);
-        this.filter('').then((results) => this.set('results', results));
+        this.filter('').then((allResults) => {
+            this.set('results', allResults.results);
+        });
     },
 
     actions: {
@@ -14,7 +16,11 @@ export default Component.extend({
             let filterInputValue = this.value;
             let filterAction = this.filter;
 
-            filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+            filterAction(filterInputValue).then((filterResults) => {
+                if (filterResults.query === this.value) {
+                    this.set('results', filterResults.results);
+                }
+            });
         }
     }
 });
